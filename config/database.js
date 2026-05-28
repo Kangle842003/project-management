de27a33c91c30d1be7a3f3efed7435f74948940e
Le Tran Phuc Khang <phuckhang842003@gmail.com>
2026-05-28 11:02:39 +0700

    kiem tra ket noi database

---
+++ config/database.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose")// khai bao thu vien mongoose
+
+
+module.exports.connect = async ()=>{
+    try {
+        await mongoose.connect(process.env.MONGOOSE_URL);
+        console.log("Connect Success!")
+    } catch (error) {
+        console.log("Connect Error!")
+    }
+}
--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express")
 require("dotenv").config() // Khai bao thu vien dotenv
-const mongoose = require("mongoose")// khai bao thu vien mongoose
+
+const database = require("./config/database")
+database.connect()
 
 const app = express(); 
 
 const route = require("./routes/client/index.route") 
 
-mongoose.connect("mongodb://localhost:27017/product-management");
 
 const port = process.env.PORT
 
