d0b578d2ad604e48c2385fb3e0782545b8dc0cc4
Le Tran Phuc Khang <phuckhang842003@gmail.com>
2026-05-28 22:20:22 +0700

    Lay data in ra giao dien

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,6 +1,16 @@
+const Product = require("../../models/product.model")
+
 // [GET] admin/products
-module.exports.index = (req,res) =>{
+module.exports.index = async (req,res) =>{
+    let find = {
+        deleted : false
+    }
+
+    const data = await Product.find(find)
+    // console.log(data)
+
     res.render("admin/pages/product/index.pug",{
+        data: data,
         pagetitle : "Trang san pham"
     })
 }
