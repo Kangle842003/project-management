c7fdb92359a8854b3aeaa8950ea79b09fc7b2e15
Le Tran Phuc Khang <phuckhang842003@gmail.com>
2026-05-28 09:05:23 +0700

    create project

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express(); 
+
+const route = require("./routes/client/index.route") 
+const port = process.env.PORT 
+
+app.set("views", "views");
+app.set("view engine", "pug"); 
+
+route(app)
+
+app.listen(port,()=>{ 
+  console.log(`server is running on http://localhost:${port}`) 
+})
