8406e110468ae4d7b5ab33c4b6a1b9215e318c20
Le Tran Phuc Khang <phuckhang842003@gmail.com>
2026-05-28 10:55:20 +0700

    Cau hình Database

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express")
 require("dotenv").config() // Khai bao thu vien dotenv
+const mongoose = require("mongoose")// khai bao thu vien mongoose
+
 const app = express(); 
 
 const route = require("./routes/client/index.route") 
 
+mongoose.connect("mongodb://localhost:27017/product-management");
+
 const port = process.env.PORT
+
 
 app.set("views", "views");
 app.set("view engine", "pug"); 
