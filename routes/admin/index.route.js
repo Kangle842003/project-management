b7d1c5a0e2b834a5af5b7cb8d8217b7d5c1c15e1
Le Tran Phuc Khang <phuckhang842003@gmail.com>
2026-05-28 22:03:47 +0700

    Làm giao dien danh sách san pham

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,10 +1,13 @@
 const systemConfig = require("../../config/systemConfig")
 
 const dashboardRoute = require("./dashboard.route")
+const productRoute = require("./product.route")
 
 module.exports = (app) =>{
 
     const PATH_ADMIN = systemConfig.prefixAdmin 
     
     app.use(`${PATH_ADMIN}/dashboard`,dashboardRoute)
+
+    app.use(`${PATH_ADMIN}/products`,productRoute)
 }
