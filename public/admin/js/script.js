bd85c21ae7657fb8b577e56cce601510711b561f
Le Tran Phuc Khang <phuckhang842003@gmail.com>
2026-06-03 22:27:48 +0700

    Chuc nang tim kiem (ben phia front-end)

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -17,3 +17,21 @@ if(buttonStatus.length > 0){
     })
 }
 //End Loc theo trang thai
+
+// Tim kiem san pham
+const formSearch = document.querySelector("#form-search")
+if(formSearch){
+    let url = new URL(window.location.href)
+    formSearch.addEventListener("submit",(e)=>{
+        e.preventDefault()
+        const keyword = e.target.elements.keyword.value
+        if(keyword){
+            url.searchParams.set("keyword",keyword)
+        }
+        else{
+            url.searchParams.delete("keyword")
+        }
+        window.location.href = url.href
+    })
+}
+// End tim kiem san pham
