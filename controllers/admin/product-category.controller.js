9edeabeb386ba43332890aba3aab9e1248e6283f
Le Tran Phuc Khang <phuckhang842003@gmail.com>
2026-06-13 22:39:55 +0700

    de quy tree ben create

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -18,10 +18,26 @@ module.exports.create = async (req,res) =>{
     let find = {
         deleted:false
     }
+    function createTree(arr, parentId = "") {
+    const tree = [];
+    arr.forEach((item) => {
+        if (item.parent_id === parentId) {
+            const newItem = item;
+            const children = createTree(arr, item.id);
+            if (children.length > 0) {
+                newItem.children = children;
+            }
+            tree.push(newItem);
+        }
+    });
+    return tree;
+    }
     const data = await productCategory.find(find)
-    console.log(data)
+    const newData = createTree(data)
+
+    // console.log(data)
     res.render("admin/pages/product-category/create",{
-        data:data,
+        data : newData,
         pagetitle:"TAO MOI DANH MUC"
     })
 }
