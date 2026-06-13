37ec6921add2e4dc73359aaebf4c6f5ecf428ab3
Le Tran Phuc Khang <phuckhang842003@gmail.com>
2026-06-13 22:56:02 +0700

    toi uu createTree

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -1,51 +1,27 @@
 const productCategory = require("../../models/product-category.model")
+const createTree = require("../../helpers/createTree.js");
 const systemConfig = require("../../config/systemConfig")
 
 //[GET] /admin/products-category
-module.exports.index = async (req,res) =>{
+
+module.exports.index = async (req, res) => {
     let find = {
-        deleted : false
-    }
-    function createTree(arr, parentId = "") {
-    const tree = [];
-    arr.forEach((item) => {
-        if (item.parent_id === parentId) {
-            const newItem = item;
-            const children = createTree(arr, item.id);
-            if (children.length > 0) {
-                newItem.children = children;
-            }
-            tree.push(newItem);
-        }
+        deleted: false
+    };
+    const data = await productCategory.find(find);
+
+    const newData = createTree(data);
+
+    res.render("admin/pages/product-category/index", {
+        data: newData,
+        pagetitle: "DANH MUC SAN PHAM"
     });
-    return tree;
-    }
-    const data = await productCategory.find(find)
-    const newData = createTree(data)
-    res.render("admin/pages/product-category/index",{
-        data:newData,
-        pagetitle:"DANH MUC SAN PHAM"
-    })
-}
+};
 
 //[GET] /admin/products-category/create
 module.exports.create = async (req,res) =>{
     let find = {
         deleted:false
-    }
-    function createTree(arr, parentId = "") {
-    const tree = [];
-    arr.forEach((item) => {
-        if (item.parent_id === parentId) {
-            const newItem = item;
-            const children = createTree(arr, item.id);
-            if (children.length > 0) {
-                newItem.children = children;
-            }
-            tree.push(newItem);
-        }
-    });
-    return tree;
     }
     const data = await productCategory.find(find)
     const newData = createTree(data)
---
+++ helpers/createTree.js
@@ -0,0 +1,17 @@
+function createTree(arr, parentId = "") {
+    const tree = [];
+    arr.forEach((item) => {
+        if (item.parent_id == parentId) {
+            const newItem = {
+                ...item._doc
+            };
+            const children = createTree(arr, item._id);
+            if (children.length > 0) {
+                newItem.children = children;
+            }
+            tree.push(newItem);
+        }
+    });
+    return tree;
+}
+module.exports = createTree;
