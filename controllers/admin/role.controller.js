91211154181d249b8dd33ea6b79f32e3ba0a450e
Le Tran Phuc Khang <phuckhang842003@gmail.com>
2026-06-16 22:02:27 +0700

    Xây dung phan Thêm moi nhóm quyen

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -1,5 +1,28 @@
+const role = require("../../models/role.model")
+const systemConfig = require("../../config/systemConfig")
+
+//[GET] admin/roles
 module.exports.index =  async (req,res) =>{
-    res.render("admin/pages/role/index.pug",{
-        pagetitle:"Trang nhom quyen"
+    const data = await role.find({
+        deleted:false
     })
+    res.render("admin/pages/role/index.pug",{
+        pagetitle:"Trang nhom quyen",
+        data:data
+    })
+}
+
+//[GET] admin/roles/create
+module.exports.create = async (req,res) =>{
+    res.render("admin/pages/role/create.pug",{
+        pagetitle:"Them nhom quyen"
+    })
+}
+
+//[POST] admin/roles/create
+module.exports.createPost = async(req,res) =>{
+    const data = new role(req.body)
+    await data.save()
+    
+    res.redirect(`${systemConfig.prefixAdmin}/roles`)
 }
