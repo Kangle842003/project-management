fe098451b2948bb4d325f30100299eaeff23cced
Le Tran Phuc Khang <phuckhang842003@gmail.com>
2026-06-25 20:10:39 +0700

    Tao collection accounts

---
+++ models/account.model.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose")
+const randomString = require("../helpers/randomString")
+
+const accountSchema = new mongoose.Schema({
+    fullName: String,
+    email: String,
+    passWord: String,
+    token:{
+        type:String,
+        default:randomString.generateRandomString(30)
+    },
+    phone:Number,
+    avatar:String,
+    role_id:String,
+    status:String,
+    deleted: {
+        type: Boolean,
+        default: false
+    },
+    deletedAt: Date,
+    permissions :{
+        type:Array,
+        default :[]
+    }
+}, {
+    timestamps: true
+})
+
+const Account = mongoose.model("Account", accountSchema, "accounts")
+
+module.exports = Account
